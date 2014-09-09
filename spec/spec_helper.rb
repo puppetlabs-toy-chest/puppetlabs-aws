f217f66f9d4326bc8c00bc7288ebad64a7d3fd15
Gareth Rushgrove <gareth@morethanseven.net>
2014-09-09 09:09:01 -0700

    initial running type specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,5 @@
+require 'aws-sdk-core'
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!
---
+++ spec/type/ec2_securitygroup_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+type_class = Puppet::Type.type(:ec2_securitygroup) 
+
+describe type_class do
+
+  let :params do
+    [
+      :name,
+      :ingress,
+      :description
+    ]
+  end
+
+  let :properties do
+    [
+      :ensure
+    ]
+  end
+
+  it 'should have expected properties' do
+    properties.each do |property|
+      type_class.properties.map { |p| p.name }.should be_include(property)
+    end
+  end
+
+  it 'should have expected parameters' do
+    params.each do |param|
+      type_class.parameters.should be_include(param)
+    end
+  end
+end
