070c736595763dd9f4229cb6bb0d7fb9a1f3311b
Gareth Rushgrove <gareth@morethanseven.net>
2014-09-10 11:51:49 -0700

    initial working version of instance provider

--- lib/puppet/provider/ec2_instance/v2.rb
+++ lib/puppet/provider/ec2_instance/v2.rb
@@ -4,17 +4,43 @@ module Puppet
   class Provider
     class Ec2Instance < Puppet::Provider
       def initialize(*args)
+        @client = Aws::EC2::Client.new(region: 'us-west-2')
         super(*args)
       end
 
+      def _find_instances
+        @client.describe_instances(filters: [
+          {name: 'tag:Name', values: [name]},
+          {name: 'instance-state-name', values: ['pending', 'running']}
+        ])
+      end
+
       def exists?
-        Puppet.debug("Checking if instance #{resource[:name]} exists")
+        Puppet.info("Checking if instance #{name} exists")
+        !_find_instances.reservations.empty?
       end
 
       def create
+        Puppet.info("Creating instance #{name}")
+        response = @client.run_instances(
+          image_id: resource[:image_id],
+          min_count: 1,
+          max_count: 1,
+          instance_type: resource[:instance_type],
+        )
+        @client.create_tags(
+          resources: response.instances.map(&:instance_id),
+          tags: [
+            {key: 'Name', value: name}
+          ]
+        )
       end
 
       def destroy
+        Puppet.info("Deleting instance #{name}")
+        @client.terminate_instances(
+          instance_ids: _find_instances.reservations.first.instances.map(&:instance_id),
+        )
       end
     end
   end
