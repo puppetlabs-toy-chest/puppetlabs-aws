a66921168f0d927352be5df54e2e89370bdf75d1
Gareth Rushgrove <gareth@morethanseven.net>
2014-09-11 09:45:56 -0700

    add stubbed elb tests

---
+++ spec/provider/elb_loadbalancer/v2_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+provider_class = Puppet::Type.type(:elb_loadbalancer).provider(:v2)
+
+ENV['AWS_ACCESS_KEY_ID'] = 'redacted'
+ENV['AWS_SECRET_ACCESS_KEY'] = 'redacted'
+
+describe provider_class do
+
+  context 'with the minimum params' do
+    before(:each) do
+      @resource = Puppet::Type.type(:elb_loadbalancer).new(
+        name: 'test',
+        instances: [],
+        listeners: [],
+        security_groups: [],
+        availability_zones: []
+      )
+      @provider = provider_class.new(@resource)
+    end
+
+    it 'should be an instance of the ProviderV2' do
+      @provider.should be_an_instance_of Puppet::Type::Elb_loadbalancer::ProviderV2
+    end
+
+    context 'exists?' do
+      it 'should correctly report non-existent load balancers' do
+      end
+
+      it 'should correctly find existing load balancers' do
+      end
+    end
+
+    context 'create' do
+    end
+
+    context 'destroy' do
+    end
+
+  end
+
+end
---
+++ spec/type/elb_loadbalancer_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+type_class = Puppet::Type.type(:elb_loadbalancer)
+
+describe type_class do
+
+  let :params do
+    [
+      :name,
+      :availability_zones,
+      :security_groups,
+      :instances,
+      :listeners
+    ]
+  end
+
+  let :properties do
+    [
+      :ensure
+    ]
+  end
+
+  it 'should have expected properties' do
+    properties.each do |property|
+      type_class.properties.map(&:name).should be_include(property)
+    end
+  end
+
+  it 'should have expected parameters' do
+    params.each do |param|
+      type_class.parameters.should be_include(param)
+    end
+  end
+end
