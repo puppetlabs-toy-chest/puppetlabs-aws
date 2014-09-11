45eff20fda8e0f98a0a1a2424549d7c6fe8ede2b
Gareth Rushgrove <gareth@morethanseven.net>
2014-09-11 14:36:34 -0700

    added additional acceptance tests for elbs

--- acceptance/puppetlabs-aws-expect/test/clojure/puppetlabs_aws_expect/test.clj
+++ acceptance/puppetlabs-aws-expect/test/clojure/puppetlabs_aws_expect/test.clj
@@ -1,5 +1,6 @@
 (ns clojure.puppetlabs-aws-expect.test
-  (:use [amazonica.aws.ec2])
+  (:use [amazonica.aws.ec2]
+        [amazonica.aws.elasticloadbalancing])
   (:require [environ.core :refer [env]]
             [expectations :refer [expect from-each]]))
 
@@ -13,12 +14,24 @@
 (def groups (:security-groups
   (describe-security-groups region)))
 
+(def groups (:security-groups
+  (describe-security-groups region)))
+
 (def group-names (map :group-name
   (:security-groups
     (describe-security-groups region))))
+
+(def loadbalancers (:load-balancer-descriptions
+  (describe-load-balancers region)))
+
+(def loadbalancer-names (map :load-balancer-name loadbalancers))
 
 (expect 4 (count groups))
 
 (expect (in? group-names "web-sg"))
 (expect (in? group-names "lb-sg"))
 (expect (in? group-names "db-sg"))
+
+(expect 1 (count loadbalancers))
+
+(expect (in? loadbalancer-names "lb-1"))
