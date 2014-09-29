64293caccf9694a7fc13bf4b83692a8c6b44a3f6
Gareth Rushgrove <gareth@morethanseven.net>
2014-09-29 12:46:02 +0100

    update AMI with latest bash

--- contrib/packer/scripts/puppet.sh
+++ contrib/packer/scripts/puppet.sh
@@ -4,9 +4,14 @@
 # interfere with package installation
 sudo apt-get install language-pack-en -y
 
+# Upgrade everything
+sudo apt-get update
+sudo apt-get upgrade -y
+
 # Install puppet from official packages
 cd /tmp
 wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
 sudo apt-get update
+
 sudo apt-get install puppet -y
