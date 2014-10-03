e06c9cba2f1964706d0f6f9188ee46b1fec80ebd
Gareth Rushgrove <gareth@morethanseven.net>
2014-10-03 11:56:23 +0100

    updte spec syntax to latest to avoid future deprecations

--- spec/unit/type/ec2_securitygroup_spec.rb
+++ spec/unit/type/ec2_securitygroup_spec.rb
@@ -21,13 +21,13 @@ describe type_class do
 
   it 'should have expected properties' do
     properties.each do |property|
-      type_class.properties.map(&:name).should be_include(property)
+      expect(type_class.properties.map(&:name)).to be_include(property)
     end
   end
 
   it 'should have expected parameters' do
     params.each do |param|
-      type_class.parameters.should be_include(param)
+      expect(type_class.parameters).to be_include(param)
     end
   end
 end
