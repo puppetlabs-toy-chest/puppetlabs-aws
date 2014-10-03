b1d189a98ad0f059aac1cd34e5d97517eba24acd
Gareth Rushgrove <gareth@morethanseven.net>
2014-10-03 12:04:29 +0100

    specify env var to catch region default

--- spec/unit/provider/elb_loadbalancer/v2_spec.rb
+++ spec/unit/provider/elb_loadbalancer/v2_spec.rb
@@ -4,6 +4,7 @@ provider_class = Puppet::Type.type(:elb_loadbalancer).provider(:v2)
 
 ENV['AWS_ACCESS_KEY_ID'] = 'redacted'
 ENV['AWS_SECRET_ACCESS_KEY'] = 'redacted'
+ENV['AWS_REGION'] = 'us-west-2'
 
 describe provider_class do
 
