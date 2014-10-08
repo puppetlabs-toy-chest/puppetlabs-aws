8fc80c8aba9e2980a374e20df0ccd46dff2a5916
Gareth Rushgrove <gareth@morethanseven.net>
2014-10-08 13:55:43 +0100

    Normalise the name of the zone, to prevent duplicates
    The AWS API automaically downcases everything and adds a period, meaning
    that without validation you can have duplicates created in AWS via
    valid resources in the DSL.

--- lib/puppet/type/route53_zone.rb
+++ lib/puppet/type/route53_zone.rb
@@ -6,6 +6,8 @@ Puppet::Type.newtype(:route53_zone) do
   newparam(:name, namevar: true) do
     desc 'the name of DNS zone group'
     validate do |value|
+      fail Puppet::Error, 'Zone names must be all lowercase' unless value.downcase == value
+      fail Puppet::Error, 'Zone names must end with a period' unless value.end_with?('.')
       fail Puppet::Error, 'Empty values are not allowed' if value == ''
     end
   end
