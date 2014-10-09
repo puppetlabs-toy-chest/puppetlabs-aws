ad86332677d5a1b2679557b966ef37934eef03a7
Gareth Rushgrove <gareth@morethanseven.net>
2014-10-09 10:40:17 +0100

    Remove the scoping of region via the ENV variable
    This appears to be at the heart of the issues you were having I think.
    This was intended to be a short term optimisation, but if it's causing
    too much uncertainty we should pull this out.
    
    We can come back to performance and questions over whether scoping or
    caching are good ideas/required for a nice user experience.
    
    This removes that problem but retains the puppet resource functionality.

--- lib/puppet_x/puppetlabs/aws.rb
+++ lib/puppet_x/puppetlabs/aws.rb
@@ -4,38 +4,26 @@ module PuppetX
   module Puppetlabs
     class Aws < Puppet::Provider
       def self.regions
-        if ENV['AWS_REGION'] and not ENV['AWS_REGION'].empty?
-          [ENV['AWS_REGION']]
-        else
-          ec2_client(region: default_region).describe_regions.data.regions.map(&:region_name)
-        end
+        ec2_client(region: 'sa-east-1').describe_regions.data.regions.map(&:region_name)
       end
 
       def regions
         self.class.regions
       end
 
-      def self.default_region
-        ENV['AWS_REGION'] || 'eu-west-1'
-      end
-
-      def default_region
-        self.class.default_region
-      end
-
-      def self.ec2_client(region: default_region)
+      def self.ec2_client(region: nil)
         ::Aws::EC2::Client.new(region: region)
       end
 
-      def ec2_client(region: default_region)
+      def ec2_client(region: nil)
         self.class.ec2_client(region: region)
       end
 
-      def self.elb_client(region: default_region)
+      def self.elb_client(region: nil)
         ::Aws::ElasticLoadBalancing::Client.new(region: region)
       end
 
-      def elb_client(region: default_region)
+      def elb_client(region: nil)
         self.class.elb_client(region: region)
       end
     end
