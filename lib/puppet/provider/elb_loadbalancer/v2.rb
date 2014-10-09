98189ac670787f414b1b47d64df7a56d9b24c67d
Iristyle <Iristyle@github>
2014-10-08 17:26:09 -0700

    Add tag support to load_balancer

--- lib/puppet/provider/elb_loadbalancer/v2.rb
+++ lib/puppet/provider/elb_loadbalancer/v2.rb
@@ -53,6 +53,7 @@ Puppet::Type.type(:elb_loadbalancer).provide(:v2, :parent => PuppetX::Puppetlabs
     zones = resource[:availability_zones]
     zones = [zones] unless zones.is_a?(Array)
 
+    tags = resource[:tags].map { |k,v| {key: k, value: v} }
     elb_client(region: resource[:region]).create_load_balancer(
       load_balancer_name: name,
       listeners: [
@@ -64,7 +65,8 @@ Puppet::Type.type(:elb_loadbalancer).provide(:v2, :parent => PuppetX::Puppetlabs
         },
       ],
       availability_zones: zones,
-      security_groups: security_groups
+      security_groups: security_groups,
+      tags: tags
     )
 
     instances = resource[:instances]
