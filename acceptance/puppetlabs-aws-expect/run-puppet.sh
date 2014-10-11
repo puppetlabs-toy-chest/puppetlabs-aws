b201de49f5492152bf82078857e9d0ad175ca427
Iristyle <Iristyle@github>
2014-10-10 17:51:45 -0700

    Don't assume Puppet in path during acceptance
     - Prefer the Puppet installed via local bundle before falling back to
       the global Puppet

--- acceptance/puppetlabs-aws-expect/run-puppet.sh
+++ acceptance/puppetlabs-aws-expect/run-puppet.sh
@@ -2,4 +2,11 @@
 
 [ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh
 
-puppet apply tests/$1 --modulepath ../../../
+# Test if we can run inside bundler
+OUTPUT=$(bundle exec puppet --version)
+
+if [[ $? == 0 ]]; then
+  bundle exec puppet apply tests/$1 --modulepath ../../../
+else
+  puppet apply tests/$1 --modulepath ../../../
+fi
