afa2e032ff3ec689ac19b262ed184a8b647ec7c7
Iristyle <Iristyle@github>
2014-10-14 13:30:54 -0700

    Update exists? message from Puppet.info
     - Prefer resource[:region], and fall back to region.  Without this
       change, when resources specified in a manifest don't exist, then
       message first appears like:
    
       Info: Checking if load balancer empty-lb exists in region absent
       Info: Creating load balancer empty-lb in region sa-east-1
    
       Followed by:
    
       Info: Checking if load balancer empty-lb exists in region sa-east-1
    
       After this change, the first message appears like:
    
       Info: Checking if load balancer empty-lb exists in region sa-east-1
       Info: Creating load balancer empty-lb in region sa-east-1

--- lib/puppet/provider/ec2_instance/v2.rb
+++ lib/puppet/provider/ec2_instance/v2.rb
@@ -48,7 +48,8 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
   end
 
   def exists?
-    Puppet.info("Checking if instance #{name} exists in region #{region}")
+    dest_region = resource[:region] if resource
+    Puppet.info("Checking if instance #{name} exists in region #{dest_region || region}")
     @property_hash[:ensure] == :present
   end
 
