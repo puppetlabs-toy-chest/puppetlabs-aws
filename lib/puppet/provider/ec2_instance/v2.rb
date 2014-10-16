12a2879d3e1f366a0b90209b8e1ea672e1451690
Gareth Rushgrove <gareth@morethanseven.net>
2014-10-16 19:33:26 +0100

    allow placing instances in subnets

--- lib/puppet/provider/ec2_instance/v2.rb
+++ lib/puppet/provider/ec2_instance/v2.rb
@@ -22,8 +22,8 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
     end.flatten
   end
 
-  read_only(:instance_id, :instance_type, :region, :user_data, :key_name,
-            :availability_zones, :security_groups, :monitoring)
+  read_only(:instance_id, :instance_type, :region, :user_data,
+            :availability_zones, :security_groups, :monitoring, :subnet)
 
   def self.prefetch(resources)
     instances.each do |prov|
@@ -40,6 +40,14 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
     instance.tags.each do |tag|
       tags[tag.key] = tag.value unless tag.key == 'Name'
     end
+
+    subnet_name = nil
+    if instance.subnet_id
+      subnet_response = ec2_client(region: region).describe_subnets(subnet_ids: [instance.subnet_id])
+      subnet_name_tag = subnet_response.data.subnets.first.tags.detect { |tag| tag.key == 'Name' }
+    end
+    subnet_name = subnet_name_tag ? subnet_name_tag.value : nil
+
     {
       name: name_tag ? name_tag.value : nil,
       instance_type: instance.instance_type,
@@ -51,6 +59,7 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
       ensure: instance.state.name.to_sym,
       tags: tags,
       region: region
+      subnet: subnet_name,
     }
   end
 
@@ -76,11 +85,17 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
     else
       data = resource[:user_data].nil? ? nil : Base64.encode64(resource[:user_data])
 
+      ec2 = ec2_client(resource[:region])
+
+      groups_response = ec2.describe_security_groups(filters: [
+        {name: "group-name", values: groups},
+      ])
+
       config = {
         image_id: resource[:image_id],
         min_count: 1,
         max_count: 1,
-        security_groups: groups,
+        security_group_ids: groups_response.data.security_groups.map(&:group_id),
         instance_type: resource[:instance_type],
         user_data: data,
         placement: {
@@ -89,18 +104,24 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
         monitoring: {
           enabled: resource[:monitoring].to_s,
         }
-      }
 
       key = resource[:key_name] ? resource[:key_name] : false
       config['key_name'] = key if key
 
-      response = ec2_client(resource[:region]).run_instances(config)
+      if resource[:subnet]
+        subnet_response = ec2.describe_subnets(filters: [
+          {name: "tag:Name", values: [resource[:subnet]]},
+        ])
+        config[:subnet_id] = subnet_response.data.subnets.first.subnet_id
+      end
+
+      response = ec2.run_instances(config)
 
       @property_hash[:ensure] = :present
 
       tags = resource[:tags] ? resource[:tags].map { |k,v| {key: k, value: v} } : []
       tags << {key: 'Name', value: name}
-      ec2_client(resource[:region]).create_tags(
+      ec2.create_tags(
         resources: response.instances.map(&:instance_id),
         tags: tags
       )
@@ -137,12 +158,13 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
 
   def tags=(value)
     Puppet.info("Updating tags for #{name} in region #{region}")
-    ec2_client(resource[:region]).create_tags(
+    ec2 = ec2_client(resource[:region])
+    ec2.create_tags(
       resources: [instance_id],
       tags: value.collect { |k,v| { :key => k, :value => v } }
     ) unless value.empty?
     missing_tags = tags.keys - value.keys
-    ec2_client(resource[:region]).delete_tags(
+    ec2.delete_tags(
       resources: [instance_id],
       tags: missing_tags.collect { |k| { :key => k } }
     ) unless missing_tags.empty?
@@ -150,15 +172,15 @@ Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
 
   def destroy
     Puppet.info("Deleting instance #{name} in region #{resource[:region]}")
-    instances = ec2_client(resource[:region]).describe_instances(filters: [
+    ec2 = ec2_client(resource[:region])
+    instances = ec2.describe_instances(filters: [
       {name: 'tag:Name', values: [name]},
       {name: 'instance-state-name', values: ['pending', 'running', 'stopping', 'stopped']}
     ])
-    ec2_client(resource[:region]).terminate_instances(
+    ec2.terminate_instances(
       instance_ids: instances.reservations.map(&:instances).
         flatten.map(&:instance_id)
     )
-
     @property_hash[:ensure] = :absent
   end
 end
--- tests/vpc.pp
+++ tests/vpc.pp
@@ -5,10 +5,11 @@ ec2_vpc { 'test-vpc':
 }
 
 ec2_vpc_subnet { 'test-subnet':
-  ensure     => present,
-  cidr_block => '10.0.0.0/24',
-  region     => 'sa-east-1',
-  vpc        => 'test-vpc',
+  ensure            => present,
+  cidr_block        => '10.0.0.0/24',
+  region            => 'sa-east-1',
+  vpc               => 'test-vpc',
+  availability_zone => 'sa-east-1a',
 }
 
 ec2_vpc_internet_gateway { 'test-gateway':
@@ -29,6 +30,7 @@ ec2_vpc_route_table { 'test-route-table':
       destination_cidr_block => '10.0.0.0/16',
       gateway                => 'local',
     }
+
   ]
 }
 
@@ -44,3 +46,17 @@ ec2_securitygroup { 'vpc-sg':
   }],
 }
 
+ec2_instance { 'vpc-1':
+  ensure            => present,
+  region            => 'sa-east-1',
+  image_id          => 'ami-6dd26770',
+  security_groups   => ['vpc-sg'],
+  availability_zone => 'sa-east-1a',
+  subnet            => 'test-subnet',
+  instance_type     => 't2.micro',
+  tags              => {
+    department => 'engineering',
+    project    => 'cloud',
+    created_by => 'garethr'
+  }
+}
