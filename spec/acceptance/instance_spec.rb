d4ba92da1a361b83a0f4e3871fd9219c680f0f69
Gareth Rushgrove <gareth@morethanseven.net>
2014-10-23 13:15:37 +0100

    Without a wait it's possible to get a race condition
    I think this is mainly a testing aberation, creating resources and
    destroying them moments later is an odd usecase so I'm not too worried
    about the sleep here. But would be nicer to thing about this a bit more
    later.

--- spec/acceptance/instance_spec.rb
+++ spec/acceptance/instance_spec.rb
@@ -16,6 +16,7 @@ describe "ec2_instance" do
   end
 
   after(:all) do
+    sleep(10) # TODO: investigate more reliable way of making sure instances terminate
     new_config = @config.update({:ensure => 'absent'})
     PuppetManifest.new(@template, new_config).apply
   end
