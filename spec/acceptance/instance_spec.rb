3b540daf052077ee6f5d34950af1e82a7241ec1c
Iristyle <Iristyle@github>
2014-10-28 15:44:57 -0700

    Refactor instance acceptance tests to sleep
     - Add need wait_until_status helper that takes an instance name and
       will wait until it's hit a given state, or else will raise a standard
       RSpec expectation exception.
     - Existing test now waits until instance is running before proceeding
       with a termination of the instance.  It additionally verifies that
       the instance enters the 'shutting-down' state.

--- spec/acceptance/instance_spec.rb
+++ spec/acceptance/instance_spec.rb
@@ -11,23 +11,47 @@ describe "ec2_instance" do
       :image_id => 'ami-41e85d5c',
       :ensure => 'present',
     }
+    @ec2 = Ec2Helper.new(@config[:region])
     @template = 'instance.pp.tmpl'
     PuppetManifest.new(@template, @config).apply
   end
 
   after(:all) do
-    sleep(3) # TODO: investigate more reliable way of making sure instances terminate
+    wait_until_status(@config[:name], 'running')
+
     new_config = @config.update({:ensure => 'absent'})
     PuppetManifest.new(@template, new_config).apply
+
+    wait_until_status(@config[:name], 'shutting-down')
+  end
+
+  def find_instance(name)
+    instances = @ec2.get_instances(name)
+    expect(instances.count).to eq(1)
+    instances.first
+  end
+
+  def wait_until_status(name, status, max_wait = 15)
+    slept = 0
+
+    loop do
+      current_status = find_instance(name).state.name
+      break if current_status == status
+
+      sleep(1)
+      slept += 1
+
+      if slept > max_wait
+        msg = "Exceeded timeout of #{max_wait} waiting for #{name} to be #{status}"
+        expect(current_status).to eq(status), msg
+      end
+    end
   end
 
   describe 'should create a new instance' do
 
     before(:all) do
-      ec2 = Ec2Helper.new(@config[:region])
-      instances = ec2.get_instances(@config[:name])
-      expect(instances.count).to eq(1)
-      @instance = instances.first
+      @instance = find_instance(@config[:name])
     end
 
     it "with the specified name" do
