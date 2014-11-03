c990161b4f33cb1aab6b5bddc3c2e085cba305b4
Gareth Rushgrove <gareth@morethanseven.net>
2014-11-03 15:55:44 +0000

    Add instances to the load balancer test

--- spec/acceptance/loadbalancer_spec.rb
+++ spec/acceptance/loadbalancer_spec.rb
@@ -7,17 +7,34 @@ describe "ec2_loadbalancer" do
     @default_region = 'sa-east-1'
     @default_availability_zone = "#{@default_region}a"
     @aws = AWSHelper.new(@default_region)
-    @template = 'loadbalancer.pp.tmpl'
+    @instance_template = 'instance.pp.tmpl'
+    @lb_template = 'loadbalancer.pp.tmpl'
   end
 
   describe 'should create a new load balancer' do
 
     before(:all) do
-      @config = {
+      @instance_config = {
+        :name => "#{PuppetManifest.env_id}-#{SecureRandom.uuid}",
+        :instance_type => 't1.micro',
+        :region => @default_region,
+        :image_id => 'ami-41e85d5c',
+        :ensure => 'present',
+        :tags => {
+          :department => 'engineering',
+          :project    => 'cloud',
+          :created_by => 'aws-acceptance'
+        }
+      }
+
+      PuppetManifest.new(@instance_template, @instance_config).apply
+      @instance = @aws.get_instance(@instance_config[:name])
+
+      @lb_config = {
         :name => "#{SecureRandom.uuid.gsub('-', '')}"[0...31], # loadbalancer has name length limit
         :region => @default_region,
         :availability_zones => [@default_availability_zone],
-        :instances => [],
+        :instances => [@instance_config[:name]],
         :listeners => [],
         :ensure => 'present',
         :tags => {
@@ -26,22 +43,24 @@ describe "ec2_loadbalancer" do
           :created_by => 'aws-acceptance'
         }
       }
-      PuppetManifest.new(@template, @config).apply
-
-      @loadbalancer = @aws.get_loadbalancer(@config[:name])
+      PuppetManifest.new(@lb_template, @lb_config).apply
+      @loadbalancer = @aws.get_loadbalancer(@lb_config[:name])
     end
 
     after(:all) do
-      new_config = @config.update({:ensure => 'absent'})
-      PuppetManifest.new(@template, new_config).apply
+      new_instance_config = @instance_config.update({:ensure => 'absent'})
+      PuppetManifest.new(@instance_template, new_instance_config).apply
+
+      new_lb_config = @lb_config.update({:ensure => 'absent'})
+      PuppetManifest.new(@lb_template, new_lb_config).apply
     end
 
     it "with the specified name" do
-      expect(@loadbalancer.load_balancer_name).to eq(@config[:name])
+      expect(@loadbalancer.load_balancer_name).to eq(@lb_config[:name])
     end
 
     it "with the specified availability zone" do
-      expect(@loadbalancer.availability_zones).to eq(@config[:availability_zones])
+      expect(@loadbalancer.availability_zones).to eq(@lb_config[:availability_zones])
     end
 
     it "not part of a VPC" do
@@ -49,8 +68,8 @@ describe "ec2_loadbalancer" do
       expect(@loadbalancer.subnets).to be_empty
     end
 
-    it "with no associated instances" do
-      expect(@loadbalancer.instances).to be_empty
+    it "with one associated instance" do
+      expect(@loadbalancer.instances.count).to eq(1)
     end
 
     it "with no associated security groups" do
