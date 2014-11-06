fa2dff629031d882da40c499d488dba73cccf7e2
Gareth Rushgrove <gareth@morethanseven.net>
2014-11-06 10:11:31 +0000

    Add a PE installation example based on work by @mrzarquon
    This mainly involved moving the original example into the new examples
    folder and removing a few hardcoded bits.
    
    I've also written a README to describe the example.

---
+++ examples/puppet-enterprise/pe_master.pp
@@ -0,0 +1,26 @@
+$pe_username = 'admin@puppetlabs.com'
+$pe_password = 'puppetlabs'
+$pe_version_string = '3.3.2'
+
+#ec2_securitygroup { 'puppet':
+#  ensure           => present,
+#  region           => 'us-west-2',
+#  description      => 'Group for testing puppet AWS module',
+#  ingress          => [{
+#    security_group => "puppet"
+#  },{
+#    protocol => 'tcp',
+#    port     => 443,
+#    cidr     => '0.0.0.0/0',
+#  }]
+#}
+
+ec2_instance { 'puppet-master':
+  ensure          => present,
+  region          => 'us-west-2',
+  image_id        => 'ami-e08efbd0',
+  instance_type   => 'm3.large',
+  monitoring      => 'true',
+  security_groups => ['puppet'],
+  user_data       => template('master-pe-userdata.erb'),
+}
