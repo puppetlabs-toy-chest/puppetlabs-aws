0052bcba881345cb22fd934916f6f77d81b87946
Dave Seff <dave@solar1.net>
2015-01-12 10:47:52 -0500

    (CLOUD-295) Added basic support for RDS instance management.
    It can create db's now. (sort of).
    
    Added the RDS example in the README
    
    I should learn github markup.
    
    Now impliments adding rds instance to security group.
    
    Can now delete RDS instances.
    
    puppet resource rds_instance is now working.
    
    Still learning as I go. :)
    
    Figured it out. Instances no longer throw errors. puppet resource rds_instance is working fine.

---
+++ lib/puppet/type/rds_instance.rb
@@ -0,0 +1,184 @@
+Puppet::Type.newtype(:rds_instance) do
+  @doc = 'type representing an RDS instance'
+
+  ensurable do
+    defaultvalues
+    aliasvalue(:available, :present)
+    newvalue(:destroy) do
+      provider.destroy
+    end
+  end
+
+  newparam(:name, namevar: true) do
+    desc 'the name of the db instance (also known as the db_instance_identifier)'
+    validate do |value|
+    end
+  end
+
+  newparam(:db_name) do
+    desc 'The meaning of this parameter differs according to the database engine you use.
+Type: String
+
+MySQL
+
+The name of the database to create when the DB instance is created. If this parameter is not specified, no database is created in the DB instance.
+
+Constraints:
+
+Must contain 1 to 64 alphanumeric characters
+Cannot be a word reserved by the specified database engine
+PostgreSQL
+
+The name of the database to create when the DB instance is created. If this parameter is not specified, no database is created in the DB instance.
+
+Constraints:
+
+Must contain 1 to 63 alphanumeric characters
+Must begin with a letter or an underscore. Subsequent characters can be letters, underscores, or digits (0-9).
+Cannot be a word reserved by the specified database engine
+Oracle
+
+The Oracle System ID (SID) of the created DB instance.
+
+Default: ORCL
+
+Constraints:
+
+Cannot be longer than 8 characters
+SQL Server
+
+Not applicable. Must be null.'
+    validate do |value|
+    end
+  end
+
+  newparam(:security_groups, :array_matching => :all) do
+    desc 'the security groups to associate the instance'
+  end
+
+  newproperty(:tags) do
+    desc 'the tags for the instance'
+  end
+
+  newproperty(:region) do
+    desc 'the region in which to launch the instance'
+    validate do |value|
+      fail 'region should not contain spaces' if value =~ /\s/
+    end
+  end
+
+  newproperty(:db_instance_class) do
+    desc 'the type to use for the instance (mysql, | postgres)'
+    validate do |value|
+      fail 'db_instance_class should not contain spaces' if value =~ /\s/
+      fail 'db_instance_class should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:availability_zone_name) do
+    desc 'the availability zone in which to place the instance'
+    validate do |value|
+      fail 'availability_zone_name should not contain spaces' if value =~ /\s/
+    end
+  end
+
+  newproperty(:engine) do
+    desc 'the type of Database for the instance( mysql, postgres, etc...)'
+    validate do |value|
+      fail 'engine type should not contains spaces' if value =~ /\s/
+      fail 'engine should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:engine_version) do
+    desc 'the version of Database for the instance'
+    validate do |value|
+      fail 'engine_version type should not contains spaces' if value =~ /\s/
+      fail 'engine_version should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:allocated_storage) do
+    desc 'The size of the DB'
+    validate do |value|
+      fail 'allocated_storage type should not contains spaces' if value =~ /\s/
+      fail 'allocated_storage should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:license_model) do
+    desc 'the license for the instance (iValid values: license-included | bring-your-own-license | general-public-license)'
+    validate do |value|
+      fail 'license_model type should not contains spaces' if value =~ /\s/
+    end
+  end
+
+  newproperty(:storage_type) do
+    desc 'The storage stype for the DB (Valid values: gp | io1  *Note: If you specify io1, you must also include a value for the Iops parameter)'
+    validate do |value|
+      fail 'storage_type type should not contains spaces' if value =~ /\s/
+    end
+  end
+
+  newproperty(:iops) do
+    desc 'The IOPS stype for the DB (minimum 1000)'
+    validate do |value|
+      fail 'iops type should not contains spaces' if value =~ /\s/
+    end
+  end
+
+  newproperty(:master_username) do
+    desc 'The main user for the DB'
+    validate do |value|
+      fail 'master_username type should not contains spaces' if value =~ /\s/
+    end
+  end
+
+  newproperty(:master_user_password) do
+    desc 'The main user Password'
+    validate do |value|
+      fail 'master_user_password should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:multi_az) do
+    desc 'The main user Password'
+    validate do |value|
+      fail 'multi_az should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:security_groups) do
+    desc 'The main user Password'
+    validate do |value|
+      fail 'security_groups should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:db_subnet_group_name) do
+    desc 'The VPC subnet for this instance.'
+    validate do |value|
+      fail 'db_subnet_group_name should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:skip_final_snapshot) do
+    desc 'Create one last snapshot.'
+    validate do |value|
+      fail 'skip_final_snapshot should not be blank' if value == ''
+    end
+  end
+
+  newproperty(:final_db_snapshot_identifier) do
+    desc 'Create one last snapshot.'
+    validate do |value|
+      fail 'final_db_snapshot_identifier should not be blank' if value == ''
+    end
+  end
+
+  autorequire(:ec2_securitygroup) do
+    groups = self[:security_groups]
+    groups.is_a?(Array) ? groups : [groups]
+  end
+
+end
