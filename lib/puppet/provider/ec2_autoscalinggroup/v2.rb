abdcdfd7a5c4eaf75b6d93a7b5aa77adb7a96709
Victor Barba <victor.barba@groupalia.com>
2016-03-04 13:21:48 +0100

    ec2_autoscalinggroup test

--- lib/puppet/provider/ec2_autoscalinggroup/v2.rb
+++ lib/puppet/provider/ec2_autoscalinggroup/v2.rb
@@ -92,7 +92,6 @@ Puppet::Type.type(:ec2_autoscalinggroup).provide(:v2, :parent => PuppetX::Puppet
     end
 
     autoscaling_client(target_region).create_auto_scaling_group(config)
-
     @property_hash[:ensure] = :present
   end
 
