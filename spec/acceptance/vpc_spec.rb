a8a4f6e72c31b4a663e1fc739893c46019a6d77b
David Schmitt <david@dasz.at>
2016-03-08 17:04:05 +0000

    Use have_attributes for VPC specs
    This produces very nice and informative messages like this:
    
      1) The AWS module when creating a new VPC environment the created VPN gateway should have attributes {:type => "ipsec.1", :availability_zone => "sa-east-1a"}
         Failure/Error:
           is_expected.to have_attributes(
             :type => @config[:vpn_type],
             :availability_zone=>@config[:subnet_availability_zone],
           )
    
           expected #<struct vpn_gateway_id="vgw-48b26855", state="available", type="ipsec.1", availability_zone=nil, vpc_attachments=[#<struct vpc_id="vpc-0c97a669", state="attached">], tags=[#<struct key="created_by", value="aws-acceptance">, #<struct key="project", value="cloud">, #<struct key="Name", value="david@zion-14408ebb-9156-4f60-8007-0bdf7c1d910e-vgw">, #<struct key="department", value="engineering">]> to have attributes {:type => "ipsec.1", :availability_zone => "sa-east-1a"} but had attributes {:type => "ipsec.1", :availability_zone => nil}
           Diff:
           @@ -1,3 +1,3 @@
           -:availability_zone => "sa-east-1a",
           +:availability_zone => nil,
            :type => "ipsec.1",
    
         # ./spec/acceptance/vpc_spec.rb:165:in `block (4 levels) in <top (required)>'

--- spec/acceptance/vpc_spec.rb
+++ spec/acceptance/vpc_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper_acceptance'
 require 'securerandom'
 
 describe "The AWS module" do
-
   before(:all) do
     @default_region = 'sa-east-1'
     @aws = AwsHelper.new(@default_region)
@@ -76,13 +75,12 @@ describe "The AWS module" do
   end
 
   describe 'when creating a new VPC environment' do
-
     before(:all) do
       @name = "#{PuppetManifest.env_id}-#{SecureRandom.uuid}"
       region = 'sa-east-1'
       @config = {
         :name => @name,
-        :lb_name => "#{PuppetManifest.env_dns_id}#{SecureRandom.uuid}".gsub('-', '')[0...31], # loadbalancer has name length limit
+        :lb_name => "#{PuppetManifest.env_dns_id}#{SecureRandom.uuid}".delete('-')[0...31], # loadbalancer has name length limit
         :region => region,
         :ensure => 'present',
         :netbios_node_type => 2,
@@ -111,13 +109,11 @@ describe "The AWS module" do
 
       @template = 'vpc.pp.tmpl'
       @result = PuppetManifest.new(@template, @config).apply
-
       @vpc = find_vpc("#{@name}-vpc")
-      @option = find_dhcp_option("#{@name}-options")
+      @dhcp_option = find_dhcp_option("#{@name}-options")
       @subnet = find_subnet("#{@name}-subnet")
       @vgw = find_vpn_gateway("#{@name}-vgw")
       @cgw = find_customer_gateway("#{@name}-cgw")
-      @sg = find_security_group("#{@name}-sg")
     end
 
     after(:all) do
@@ -125,7 +121,6 @@ describe "The AWS module" do
       template = 'vpc_delete.pp.tmpl'
       PuppetManifest.new(template, new_config).apply
     end
-
 
     it 'should run successfully first time with changes' do
       expect(@result.exit_code).to eq(2)
@@ -136,111 +131,158 @@ describe "The AWS module" do
       expect(result.exit_code).to eq(0)
     end
 
-    it 'should create a VPC' do
-      expect(@vpc).not_to be_nil
-      expect(@vpc.instance_tenancy).to eq(@config[:vpc_instance_tenancy])
-      expect(@vpc.cidr_block).to eq(@config[:vpc_cidr])
-      expect(@vpc.dhcp_options_id).to eq(@option.dhcp_options_id)
+    describe 'the created VPC' do
+      let(:subject) { @vpc }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :instance_tenancy => @config[:vpc_instance_tenancy],
+          :cidr_block => @config[:vpc_cidr],
+          :dhcp_options_id => @dhcp_option.dhcp_options_id,
+        )
+      end
     end
 
-    it 'should create a DHCP option set' do
-      node_type = @option.dhcp_configurations.find { |conf| conf.key == 'netbios-node-type' }
-      expect(@option).not_to be_nil
-      expect(node_type.values.first.value.to_i).to eq(@config[:netbios_node_type])
-      expect(@aws.tag_difference(@option, @config[:tags])).to be_empty
+    describe 'the created DHCP option set' do
+      it("should exist") { expect(@dhcp_option).not_to be_nil }
+      it("should have the requested tags") { expect(@aws.tag_difference(@dhcp_option, @config[:tags])).to be_empty }
+      it "should be of netbios-node-type" do
+        netbios_node_type = @dhcp_option.dhcp_configurations.find { |conf| conf.key == 'netbios-node-type' }.values.first.value.to_i
+        expect(netbios_node_type).to eq(@config[:netbios_node_type])
+      end
     end
 
-    it 'should create a VPC associated security group' do
-      expect(@sg).not_to be_nil
-      expect(@sg.vpc_id).to eq(@vpc.vpc_id)
-      expect(@aws.tag_difference(@sg, @config[:tags])).to be_empty
+    describe 'the created security group' do
+      before(:all) { @sg = find_security_group("#{@name}-sg") }
+      let(:subject) { @sg }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :vpc_id => @vpc.vpc_id,
+        )
+      end
+      it("should have the requested tags") { expect(@aws.tag_difference(@sg, @config[:tags])).to be_empty }
     end
 
-    it 'should create a route table' do
-      table = find_route_table("#{@name}-routes")
-      expect(table).not_to be_nil
-      expect(table.vpc_id).to eq(@vpc.vpc_id)
-      expect(table.associations.size).to eq(1)
-      expect(table.associations.first.subnet_id).to eq(@subnet.subnet_id)
-      expect(@aws.tag_difference(table, @config[:tags])).to be_empty
+    describe 'the created route table' do
+      before(:all) { @table = find_route_table("#{@name}-routes") }
+      let(:subject) { @table }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :vpc_id => @vpc.vpc_id,
+        )
+      end
+      it("should be attached exactly one subnet") { expect(@table.associations.size).to eq(1) }
+      it("should be attached to the right subnet") { expect(@table.associations.first.subnet_id).to eq(@subnet.subnet_id) }
+      it("should have the requested tags") { expect(@aws.tag_difference(@table, @config[:tags])).to be_empty }
     end
 
-    it 'should create a subnet' do
-      expect(@subnet).not_to be_nil
-      expect(@subnet.vpc_id).to eq(@vpc.vpc_id)
-      expect(@subnet.cidr_block).to eq(@config[:subnet_cidr])
-      expect(@subnet.availability_zone).to eq(@config[:subnet_availability_zone])
-      expect(@subnet.map_public_ip_on_launch).to eq(@config[:map_public_ip_on_launch])
-      expect(@subnet.default_for_az).to be_falsy
-      expect(@aws.tag_difference(@subnet, @config[:tags])).to be_empty
+    describe 'the created subnet' do
+      let(:subject) { @subnet }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :vpc_id => @vpc.vpc_id,
+          :cidr_block => @config[:subnet_cidr],
+          :availability_zone => @config[:subnet_availability_zone],
+          :map_public_ip_on_launch => @config[:map_public_ip_on_launch],
+          :default_for_az => false,
+        )
+      end
+      it("should have the requested tags") { expect(@aws.tag_difference(@subnet, @config[:tags])).to be_empty }
     end
 
-    it 'should create a load balancer in the VPC' do
-      lb = find_load_balancer(@config[:lb_name])
-      expect(lb.vpc_id).to eq(@vpc.vpc_id)
-      expect(lb.subnets).to eq([@subnet.subnet_id])
-      expect(lb.availability_zones).to eq([@config[:subnet_availability_zone]])
-      expect(lb.instances.size).to eq(1)
-      expect(lb.security_groups).not_to be_empty
-      expect(lb.scheme).to eq(@config[:lb_scheme])
+    describe 'the created load balancer' do
+      before(:all) { @lb = find_load_balancer(@config[:lb_name]) }
+      let(:subject) { @lb }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :vpc_id => @vpc.vpc_id,
+          :subnets => [@subnet.subnet_id],
+          :availability_zones => [@config[:subnet_availability_zone]],
+          :scheme => @config[:lb_scheme],
+        )
+      end
+      it("should have exactly one instance") { expect(@lb.instances.size).to eq(1) }
+      it("should have security groups") { expect(@lb.security_groups).not_to be_empty }
     end
 
-    it 'should create a VPN gateway' do
-      expect(@vgw.type).to eq(@config[:vpn_type])
-      expect(@vgw.vpc_attachments.size).to eq(1)
-      expect(@vgw.vpc_attachments.first.vpc_id).to eq(@vpc.vpc_id)
-      expect(@vgw.availability_zone).to be_nil
-      expect(@aws.tag_difference(@vgw, @config[:tags])).to be_empty
+    describe 'the created VPN gateway' do
+      let(:subject) { @vgw }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :type => @config[:vpn_type],
+          :availability_zone => nil,
+        )
+      end
+
+      it("should be attached exactly one VPC") { expect(@vgw.vpc_attachments.size).to eq(1) }
+      it("should be attached to the right VPC") { expect(@vgw.vpc_attachments.first.vpc_id).to eq(@vpc.vpc_id) }
+      it("should have the requested tags") { expect(@aws.tag_difference(@vgw, @config[:tags])).to be_empty }
     end
 
-    it 'should create an internet gateway' do
-      igw = find_internet_gateway("#{@name}-igw")
-      expect(igw.attachments.size).to eq(1)
-      expect(igw.attachments.first.vpc_id).to eq(@vpc.vpc_id)
-      expect(@aws.tag_difference(igw, @config[:tags])).to be_empty
+    describe 'the created internet gateway' do
+      before(:all) { @igw = find_internet_gateway("#{@name}-igw") }
+      let(:subject) { @igw }
+      it { is_expected.not_to be_nil }
+      it("should be attached exactly one VPC") { expect(@igw.attachments.size).to eq(1) }
+      it("should be attached to the right VPC") { expect(@igw.attachments.first.vpc_id).to eq(@vpc.vpc_id) }
+      it("should have the requested tags") { expect(@aws.tag_difference(@igw, @config[:tags])).to be_empty }
     end
 
-    it 'should create an customer gateway' do
-      expect(@cgw.type).to eq(@config[:vpn_type])
-      expect(@cgw.ip_address).to eq(@config[:customer_ip_address])
-      expect(@cgw.bgp_asn).to eq(@config[:bgp_asn])
-      expect(@aws.tag_difference(@cgw, @config[:tags])).to be_empty
+    describe 'the created customer gateway' do
+      let(:subject) { @cgw }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :type => @config[:vpn_type],
+          :ip_address => @config[:customer_ip_address],
+          :bgp_asn => @config[:bgp_asn],
+        )
+      end
+      it("should have the requested tags") { expect(@aws.tag_difference(@cgw, @config[:tags])).to be_empty }
     end
 
-    it 'should create a VPN' do
-      vpn = find_vpn("#{@name}-vpn")
-      expect(vpn.type).to eq(@config[:vpn_type])
-      expect(vpn.vpn_gateway_id).to eq(@vgw.vpn_gateway_id)
-      expect(vpn.customer_gateway_id).to eq(@cgw.customer_gateway_id)
-      expect(vpn.routes.size).to eq(1)
-      expect(vpn.routes.first.destination_cidr_block).to eq(@config[:vpn_route])
-      expect(vpn.options.static_routes_only).to eq(@config[:static_routes])
-      expect(@aws.tag_difference(vpn, @config[:tags])).to be_empty
+    describe 'the created VPN' do
+      before(:all) { @vpn = find_vpn("#{@name}-vpn") }
+      let(:subject) { @vpn }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :type => @config[:vpn_type],
+          :vpn_gateway_id => @vgw.vpn_gateway_id,
+          :customer_gateway_id => @cgw.customer_gateway_id,
+        )
+      end
+      it("should have exactly one route") { expect(@vpn.routes.size).to eq(1) }
+      it("should have the right route") { expect(@vpn.routes.first.destination_cidr_block).to eq(@config[:vpn_route]) }
+      it("should be configured with static routes") { expect(@vpn.options.static_routes_only).to eq(@config[:static_routes]) }
+      it("should have the requested tags") { expect(@aws.tag_difference(@vpn, @config[:tags])).to be_empty }
     end
 
-    it 'should create a private instance in the VPC' do
-      instance = find_instance("#{@name}-instance")
-      expect(instance.subnet_id).to eq(@subnet.subnet_id)
-      expect(instance.vpc_id).to eq(@vpc.vpc_id)
-      expect(instance.public_ip_address).to be_nil
+    describe 'the created private instance' do
+      before(:all) { @instance = find_instance("#{@name}-instance") }
+      let(:subject) { @instance }
+      it { is_expected.not_to be_nil }
+      it do
+        is_expected.to have_attributes(
+          :subnet_id => @subnet.subnet_id,
+          :vpc_id => @vpc.vpc_id,
+          :public_ip_address => nil,
+        )
+      end
     end
 
-    context 'change tags' do
+    context 'when changing tags' do
       before(:all) do
-        expect(@aws.tag_difference(@vpc, @config[:tags])).to be_empty
-        expect(@aws.tag_difference(@option, @config[:tags])).to be_empty
-        expect(@aws.tag_difference(@subnet, @config[:tags])).to be_empty
-        expect(@aws.tag_difference(@vgw, @config[:tags])).to be_empty
-        expect(@aws.tag_difference(@cgw, @config[:tags])).to be_empty
-        expect(@aws.tag_difference(@vpc, @config[:tags])).to be_empty
-        expect(@aws.tag_difference(@vpc, @config[:tags])).to be_empty
-        expect(@aws.tag_difference(@vpc, @config[:tags])).to be_empty
-        tags = {
+        @new_config = @config.dup.update({
           :department => 'engineering',
           :created_by => 'aws-acceptance',
           :foo => 'bar',
-        }
-        @new_config = @config.dup.update(tags)
+        })
         PuppetManifest.new(@template, @new_config).apply
       end
 
@@ -278,13 +320,10 @@ describe "The AWS module" do
         vpn = find_vpn("#{@name}-vpn")
         expect(@aws.tag_difference(vpn, @new_config[:tags])).to be_empty
       end
-
     end
-
   end
 
   describe 'creating a new VPC environment with all possible properties' do
-
     before(:all) do
       @name = "#{PuppetManifest.env_id}-#{SecureRandom.uuid}"
       ip_address = generate_ip
@@ -369,14 +408,12 @@ describe "The AWS module" do
     end
 
     context 'using puppet resource' do
-
       before(:all) do
         result = PuppetManifest.new(@template, @config).apply
         expect(result.stderr).not_to match(/error/i)
       end
 
       context 'to describe an ec2_vpc' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-vpc"}
@@ -409,11 +446,9 @@ describe "The AWS module" do
           regex = /dhcp_options\s*=>\s*'#{@name}-options'/
           expect(@result.stdout).to match(regex)
         end
-
       end
 
       context 'to describe an ec2_vpc_dhcp_options' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-options"}
@@ -464,11 +499,9 @@ describe "The AWS module" do
             expect(@result.stdout).to match(regex)
           end
         end
-
       end
 
       context 'to describe an ec2_vpc_routetable' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-routes"}
@@ -498,11 +531,9 @@ describe "The AWS module" do
             expect(@result.stdout).to match(regex)
           end
         end
-
       end
 
       context 'to describe an ec2_vpc_subnet' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-subnet"}
@@ -540,11 +571,9 @@ describe "The AWS module" do
           regex = /route_table\s*=>\s*'#{@name}-routes'/
           expect(@result.stdout).to match(regex)
         end
-
       end
 
       context 'to describe an ec2_vpc_internet_gateway' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-igw"}
@@ -567,11 +596,9 @@ describe "The AWS module" do
           regex = /vpc\s*=>.*'#{@config[:igw_vpc_setting]}'/
           expect(@result.stdout).to match(regex)
         end
-
       end
 
       context 'to describe an ec2_vpc_customer_gateway' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-cgw"}
@@ -604,11 +631,9 @@ describe "The AWS module" do
           regex = /type\s*=>\s*'#{@config[:vpn_type]}'/
           expect(@result.stdout).to match(regex)
         end
-
       end
 
       context 'to describe an ec2_vpc_vpn' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-vpn"}
@@ -653,11 +678,9 @@ describe "The AWS module" do
             expect(@result.stdout).to match(regex)
           end
         end
-
       end
 
       context 'to describe an ec2_vpc_vpn_gateway' do
-
         before(:all) do
           ENV['AWS_REGION'] = @default_region
           options = {:name => "#{@name}-vgw"}
@@ -685,15 +708,11 @@ describe "The AWS module" do
           regex = /type\s*=>\s*'#{@config[:vpn_type]}'/
           expect(@result.stdout).to match(regex)
         end
-
       end
-
     end
-
   end
 
   describe 'Negative cases for VPC' do
-
     before(:all) do
       @template = 'vpc_complete.pp.tmpl'
       name = "#{PuppetManifest.env_id}-#{SecureRandom.uuid}"
@@ -810,11 +829,9 @@ describe "The AWS module" do
       regex = /Error: routes property is read-only once ec2_vpc_routetable created/
       expect(result2.stderr).to match(regex)
     end
-
   end
 
   describe 'deleteing all types with puppet resource' do
-
     before(:all) do
       name = "#{PuppetManifest.env_id}-#{SecureRandom.uuid}"
       region = @default_region
@@ -942,7 +959,5 @@ describe "The AWS module" do
       expect(result.stderr).not_to match(/Error/), error_message
       expect(@aws.send('get_dhcp_options', options_name)).to be_empty
     end
-
   end
-
 end
