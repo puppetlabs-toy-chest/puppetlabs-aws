6dc2dc109c3cfd833eb28e0e9c3c02ebc67c1acc
Zbynek Vymazal <zv@exerp.com>
2016-10-24 15:07:10 +0200

    Remove requirement for unique gateway in route table

--- lib/puppet/type/ec2_vpc_routetable.rb
+++ lib/puppet/type/ec2_vpc_routetable.rb
@@ -47,9 +47,7 @@ Puppet::Type.newtype(:ec2_vpc_routetable) do
   validate do
     routes = self[:routes]
     if routes
-      uniq_gateways = Array(routes).collect { |route| route['gateway'] }.uniq
       uniq_blocks = Array(routes).collect { |route| route['destination_cidr_block'] }.uniq
-      fail 'Only one route per gateway allowed' unless uniq_gateways.size == Array(routes).size
       fail 'destination_cidr_block must be unique' unless uniq_blocks.size == Array(routes).size
     end
   end
