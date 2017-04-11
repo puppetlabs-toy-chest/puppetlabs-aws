c2efe94986543496cbbeb857387cb9efedd793f5
Dave Seff <David.Seff@racq.com.au>
2017-04-11 14:42:21 +1000

    Add copy_tags_to_snapshot parameter

--- lib/puppet/provider/rds_instance/v2.rb
+++ lib/puppet/provider/rds_instance/v2.rb
@@ -71,6 +71,7 @@ Puppet::Type.type(:rds_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
       db_security_groups: instance.db_security_groups.collect(&:db_security_group_name),
       vpc_security_groups: vpc_security_groups,
       backup_retention_period: instance.backup_retention_period,
+      copy_tags_to_snapshot: instance.copy_tags_to_snapshot,
       availability_zone: instance.availability_zone,
       arn: instance.db_instance_arn
     }
@@ -182,6 +183,7 @@ Puppet::Type.type(:rds_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aw
       db_parameter_group_name: resource[:db_parameter_group],
       vpc_security_group_ids: vpc_sg_ids,
       backup_retention_period: resource[:backup_retention_period],
+      copy_tags_to_snapshot: resource[:copy_tags_to_snapshot],
       availability_zone: resource[:availability_zone],
       tags: tags,
     }
