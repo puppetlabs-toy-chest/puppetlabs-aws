b30d999b6534f7a97abfb183f316822001da5eda
Carl Caum <carl@carlcaum.com>
2017-11-29 11:15:25 -0800

    Load pry only if present
    Previous to this commit, the pry gem was loaded as part of the v2 provider for
    ec2_instance. This breaks on any system without pry installed. This commit
    attempts to load pry and then continues if it recieves a load error.

--- lib/puppet/provider/ec2_instance/v2.rb
+++ lib/puppet/provider/ec2_instance/v2.rb
@@ -1,6 +1,11 @@
 require_relative '../../../puppet_x/puppetlabs/aws.rb'
 require 'base64'
-require 'pry'
+
+begin
+  require 'pry'
+rescue Gem::LoadError
+  # not installed
+end
 
 Puppet::Type.type(:ec2_instance).provide(:v2, :parent => PuppetX::Puppetlabs::Aws) do
   confine feature: :aws
